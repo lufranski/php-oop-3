00b48b99f5350f221c4ab3825ee23cc1d9e24aa4
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-26 17:23:52 +0100

    Employee class created

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
         public function getLastname(){
 
-            return $this -> $cognome;
+            return $this -> cognome;
         }
         public function setLastname($cognome){
 
@@ -73,11 +73,39 @@
 
         public function getHtml(){
 
-            return $this -> getFullName() . ' ' . $this -> getBirthDate() . ' ' . $this -> getBirthDate() . ' ' . $this -> getBirthPlace() . ' ' . $this -> getFiscalCode(); 
+            return $this -> getFullName() . ' ' . $this -> getBirthDate() . ' ' . $this -> getBirthPlace() . ' ' . $this -> getFiscalCode(); 
+        }
+    }
+
+    class Impiegato extends Persona {
+
+        private $dataDiAssunzione;
+
+        public function __construct($nome,$cognome,$dataDiNascita,$luogoDiNascita,$codiceFiscale, $dataDiAssunzione){
+
+            parent :: __construct($nome,$cognome,$dataDiNascita,$luogoDiNascita,$codiceFiscale, $dataDiAssunzione);
+
+            $this -> setHiringDate($dataDiAssunzione);
+        }
+
+        public function getHiringDate(){
+
+            return $this -> dataDiAssunzione;
+        }
+        public function setHiringDate($dataDiAssunzione){
+
+            $this -> dataDiAssunzione = $dataDiAssunzione;
+        }
+
+        public function getHtml(){
+
+            return '<br>' . parent :: getHtml() . ' ' . $this -> getHiringDate();
         }
     }
 
     $persona = new Persona ('Mario', 'Rossi', '12-01-1990', 'Busto Arsizio', 'MRRSS29A90C352I');
+    $impiegato = new Impiegato ('Franco', 'Lerda', '13-02-1984', 'Modena', 'FRCLRD45A86C352J', '12-06-99');
 
     echo $persona -> getHtml();
+    echo $impiegato -> getHtml();
 ?>
